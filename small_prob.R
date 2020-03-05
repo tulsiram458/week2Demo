c21d19353bdee73fb52c1ffccdad9c4a2457df36
tulsiram405 <57439589+tulsiram405@users.noreply.github.com>
2020-03-05 12:16:25 +0000

    esrestr

--- small_prob.R
+++ small_prob.R
@@ -1,4 +1,14 @@
 vec1 <- c("Hockey", "football", "baseball", "curling", "rugby", "hurling", "basketbal", "tennis", "cricket", "lacrosse")
-vec2 <- c("hockey", "lacrosse", "hockey", "water polo", "hockey", "lacrose")
-vec3 <- c(vec1[1,3,6] + vec2 [1,3,6])
+
+vec1
+
+vec2 <- c(vec1, "hockey", "lacrosse", "hockey", "water polo", "hockey", "lacrose")
+
+vec2
+
+vec3 <- vec2 [c(1,3,6)]  
+
 vec3
+ ls()
+ 
+ 
