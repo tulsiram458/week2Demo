3831dade02eb2283528b65f3a390825d0839d8d0
tulsiram405 <57439589+tulsiram405@users.noreply.github.com>
2020-03-05 12:14:36 +0000

    small problem

---
+++ small_prob.R
@@ -0,0 +1,4 @@
+vec1 <- c("Hockey", "football", "baseball", "curling", "rugby", "hurling", "basketbal", "tennis", "cricket", "lacrosse")
+vec2 <- c("hockey", "lacrosse", "hockey", "water polo", "hockey", "lacrose")
+vec3 <- c(vec1[1,3,6] + vec2 [1,3,6])
+vec3
